c28f4bbea6e45cfa1c1e8725eb663e6a4775f9e4
anurag gharat <anuraggharat55@gmail.com>
2019-12-30 22:51:34 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,117 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './Header'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.scss';
+
+class App extends React.Component{
+
+  constructor(props){
+    
+    super(props)
+    this.state ={
+      newItem:"",
+      list:[]
+    }
+    this.addItem=this.addItem.bind(this)
+    this.updateInput=this.updateInput.bind(this)
+    this.deleteItem=this.deleteItem.bind(this)
+  }
+
+  addItem(todoValue){
+    if (todoValue !== ""){
+        const newItem = {
+          id:Date.now(),
+          value:todoValue,
+          isDone:false
+        };
+        const list = [...this.state.list];
+        list.push(newItem);
+        this.setState({
+          list,
+          newItem : ""
+        });
+      }
+  }
+  deleteItem(id){
+    const list =[...this.state.list];
+    const updatedlist =list.filter(item => item.id !== id);
+    this.setState({
+      list:updatedlist
+    });
+  }
+  updateInput(input)
+  {
+    this.setState(
+      {
+        newItem:input
+      }
+    )
+  }
+  checkItem(id){
+    this.setState(prevState => {
+      const updatedTodos = prevState.list.map(todo => {
+          if (todo.id === id) {
+              return {
+                  ...todo,
+                  isDone: !todo.isDone
+              }
+          }
+          return todo
+      })
+      return {
+          list: updatedTodos
+      }
+  })
+  
 }
+
+  
+  render(){
+    const complete={color:"red"}
+    const incomplete={color:"blue"}
+      return (
+        <div className="container">
+          <Header />
+          <div className="body">
+            <div class="card">
+                <input 
+                type="text" 
+                name="todotask"
+                placeholder = "Add a new task"
+                required
+                
+                
+                onChange={e => this.updateInput(e.target.value)}
+                value={this.state.newItem}>
+                </input>
+                <button 
+                className="add-btn"
+                onClick={() => this.addItem(this.state.newItem)}> 
+                Add
+                </button>
+            </div>
+            <div className="card">
+            <h1>
+                TASKS TO BE DONE
+            </h1>
+            <ul>
+              {this.state.list.map(item =>{
+                return (
+                <li key ={item.id} style={item.isDone ? complete : incomplete} ><input type="checkbox" checked={item.isDone} onChange={()=>this.checkItem(item.id)}></input>{item.value}<button className="delete-btn" onClick={() => this.deleteItem(item.id)}>delete</button></li>
+                )}
+                )}
+                
+            </ul>
+            </div>
+
+        </div>
+        </div>
+
+
+    )}
+}
+
+
+
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from './logo.png'
+function Header(){
+
+    return(
+        <div className="header">
+        <h1>TODOLIST APP</h1>
+        <img src={logo} ></img>
+        </div>
+    )
+
+}
+export default Header
